75afc1ec607dfc6df7a17a0076735b0b0b507df0
Seb_Breton <sebbreton54@gmail.com>
2024-08-08 17:49:01 +0200

    routes search by category

--- routes/categories.js
+++ routes/categories.js
@@ -4,8 +4,10 @@ const mongoose = require('mongoose');
 
 
 const Category = require('../models/categories');
+const Artwork = require('../models/artworks');
 const { checkBody } = require('../modules/checkBody');
 
+// Route pour obtenir toutes les catégories//////////////////////////////////
 router.get('/', (req, res)=>{
     Category.find()
     .then(categories => { 
@@ -16,5 +18,20 @@ router.get('/', (req, res)=>{
     })
 })
 
+// Route pour obtenir les œuvres d'art par catégorie///////////////////////////
+router.get('/:categoryId/artworks', (req, res) => {
+    const { category } = req.params;
+    Artwork.find({ category })
+        .then(artworks => {
+            if (artworks.length === 0) {
+                res.status(404).json({ result: false, error: 'No artworks found for this category' });
+                return;
+            }
+            res.json({ result: true, artworks });
+        })
+        .catch(error => {
+            res.status(500).json({ result: false, error: 'Error fetching artworks', error });
+        });
+});
 
 module.exports = router;
