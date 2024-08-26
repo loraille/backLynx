329595ce986791820c60ea1117a91d50147fde86
Laurent <lo.rail@proton.me>
2024-08-26 14:48:56 +0200

    correct routes categories.js search.js

--- routes/categories.js
+++ routes/categories.js
@@ -1,37 +1,19 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
+const Category = require("../models/categories");
+const Artwork = require("../models/artworks");
+const { checkBody } = require("../modules/checkBody");
 
-const Category = require('../models/categories');
-const Artwork = require('../models/artworks');
-const { checkBody } = require('../modules/checkBody');
-
-// Route pour obtenir toutes les catégories//////////////////////////////////
-router.get('/', (req, res)=>{
-    Category.find()
-    .then(categories => { 
-        console.log("#Number of categories",categories.length);
-        res.json({
-            categories,
-        });
-    })
-})
-
-// Route pour obtenir les œuvres d'art par catégorie///////////////////////////
-router.get('/:categoryId/artworks', (req, res) => {
-    const { category } = req.params;
-    Artwork.find({ category })
-        .then(artworks => {
-            if (artworks.length === 0) {
-                res.status(404).json({ result: false, error: 'No artworks found for this category' });
-                return;
-            }
-            res.json({ result: true, artworks });
-        })
-        .catch(error => {
-            res.status(500).json({ result: false, error: 'Error fetching artworks', error });
-        });
+/////// get catégories infos//////////////////////////////////
+router.get("/", (req, res) => {
+  Category.find().then((categories) => {
+    console.log("#Number of categories", categories.length);
+    res.json({
+      categories,
+    });
+  });
 });
 
 module.exports = router;
--- routes/searchs.js
+++ routes/searchs.js
@@ -1,73 +1,79 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const Artwork = require('../models/artworks')
-const Tag = require('../models/tags')
-const User = require('../models/users')
+const Artwork = require("../models/artworks");
+const Tag = require("../models/tags");
+const User = require("../models/users");
 
+///////////////////search artworks with inside//////////////////////////////////////////
+router.get("/artworks", async (req, res) => {
+  const query = req.query.title;
+  if (!query) {
+    return res
+      .status(400)
+      .json({ error: 'Query parameter "title" is required' });
+  }
+  try {
+    // Rechercher les oeuvres d'art dont le titre contient le texte spécifiée
+    const results = await Artwork.find({
+      title: { $regex: query, $options: "i" },
+    });
 
-//recherche des oeuvres d'art dont le nom contient//////////////////////////////////////////
-router.get('/artworks', async (req, res) => {
-    const query = req.query.title;
-    if (!query) {
-        return res.status(400).json({ error: 'Query parameter "title" is required' });
-    }
-    try {
-        // Rechercher les oeuvres d'art dont le titre contient le texte spécifiée
-        const results = await Artwork.find({
-            title: { $regex: query, $options: 'i' }
-        });
-
-        res.json(results);
-    } catch (error) {
-        res.status(500).json({ error: 'An error occurred while fetching artworks' });
-    }
+    res.json(results);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "An error occurred while fetching artworks" });
+  }
 });
-//recherche des oeuvres dont le tag commence par////////////////////////////////////////////////
-router.get('/tags', async (req, res) => {
-    const query = req.query.tag;
-    if (!query) {
-        return res.status(400).json({ error: 'Query parameter "tag" is required' });
-    }
+//search artworks with tag begin by////////////////////////////////////////////////
+router.get("/tags", async (req, res) => {
+  const query = req.query.tag;
+  if (!query) {
+    return res.status(400).json({ error: 'Query parameter "tag" is required' });
+  }
 
-    try {
-        // Rechercher les tags dont le nom commence par la chaîne de caractères spécifiée
-        const tags = await Tag.find({
-            name: { $regex: `^${query}`, $options: 'i' }
-        });
+  try {
+    // search tag begin with
+    const tags = await Tag.find({
+      name: { $regex: `^${query}`, $options: "i" },
+    });
 
-        // Extraire les IDs des tags trouvés
-        const tagIds = tags.map(tag => tag._id);
+    //extract id
+    const tagIds = tags.map((tag) => tag._id);
 
-        // Rechercher les œuvres d'art dont les tags contiennent les IDs trouvés
-        const results = await Artwork.find({
-            tags: { $in: tagIds }
-        });
+    // search artworks with tag id
+    const results = await Artwork.find({
+      tags: { $in: tagIds },
+    });
 
-
-        res.json(results);
-    } catch (error) {
-        res.status(500).json({ error: 'An error occurred while fetching artworks' });
-    }
+    res.json(results);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "An error occurred while fetching artworks" });
+  }
 });
 
-//recherche des artistes dont le nom contient////////////////////////////////////////////////
-router.get('/artists', async (req, res) => {
-    const query = req.query.username;
-    if (!query) {
-        return res.status(400).json({ error: 'Query parameter "username" is required' });
-    }
-    try {
-        // Rechercher les user dont le nom contient le texte spécifié
-        const results = await User.find({
-            username: { $regex: query, $options: 'i' }
-        });
+/////////////////search artists with ////////////////////////////////////////////////
+router.get("/artists", async (req, res) => {
+  const query = req.query.username;
+  if (!query) {
+    return res
+      .status(400)
+      .json({ error: 'Query parameter "username" is required' });
+  }
+  try {
+    // search user contain
+    const results = await User.find({
+      username: { $regex: query, $options: "i" },
+    });
 
-        res.json(results);
-    } catch (error) {
-        res.status(500).json({ error: 'An error occurred while fetching users' });
-    }
+    res.json(results);
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred while fetching users" });
+  }
 });
 
 module.exports = router;
