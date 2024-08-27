8b637aaf7264b0667f497c661b215a40a4ef741d
Laurent <lo.rail@proton.me>
2024-08-27 17:47:54 +0200

    app

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-require('dotenv').config()
-require('./models/connection')
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require("dotenv").config();
+require("./models/connection");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var artworksRouter = require('./routes/artworks');
-var categoriesRouter = require('./routes/categories');
-var searchRouter = require('./routes/searchs');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var artworksRouter = require("./routes/artworks");
+var categoriesRouter = require("./routes/categories");
+var searchRouter = require("./routes/searchs");
 
 var app = express();
-const fileUpload = require('express-fileupload');
-app.use(fileUpload());
-
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 
-app.use(logger('dev'));
+const fileUpload = require("express-fileupload");
+app.use(fileUpload());
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/artworks', artworksRouter)
-app.use('/categories', categoriesRouter)
-app.use('/searchs', searchRouter)
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/artworks", artworksRouter);
+app.use("/categories", categoriesRouter);
+app.use("/searchs", searchRouter);
 module.exports = app;
